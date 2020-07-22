b0795243ef8acd68f69818353acf979a7353c501
mirolyubovaalyona <56440546+mirolyubovaalyona@users.noreply.github.com>
2020-07-22 14:35:45 +1000

    тупа разделение на страницы

--- mainPage/urls.py
+++ mainPage/urls.py
@@ -4,7 +4,14 @@ from .views import index
 from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('users', views.users, name='users'),
+    path('news', views.news, name='news'),
+    path('conference', views.conference, name='conference'),
+    path('ads', views.ads, name='ads'),
+    path('polls', views.polls, name='polls'),
 ]
--- mainPage/views.py
+++ mainPage/views.py
@@ -12,3 +12,25 @@ def index(request):
 
     return render(request, "index.html", {"users": users, "news": news, "conference": conference, "ads": ads, "polls": poll})
 
+def users(request):
+
+    users=Profile.objects.all()
+    return render(request, 'users.html', {'users':users})
+
+def news(request):
+    news=News.objects.all()
+    return render(request, 'news.html', {'news':news})
+
+def conference(request):
+    conference=Conference.objects.all()
+    return render(request, 'conference.html', {'conference':conference})
+
+def ads(request):
+    ads=Ads.objects.all()
+    return render(request, 'ads.html', {'ads':ads})
+
+def polls(request):
+    polls=Poll.objects.all()
+    return render(request, 'polls.html', {'polls':polls})
+
+
